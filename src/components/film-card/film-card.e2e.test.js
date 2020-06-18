639ab1627f14dd62cd0db9693de4c71643f0aa95
AlexyShu <Za9i@bk.ru>
2020-06-18 15:55:15 +0300

    поменяла моковые данные в тестах для film-card

--- src/components/film-card/film-card.e2e.test.js
+++ src/components/film-card/film-card.e2e.test.js
@@ -7,44 +7,11 @@ Enzyme.configure({
   adapter: new Adapter(),
 });
 
-const mockFilms = [
-  {
-    name: `Fantastic Beasts`,
-    pictureSrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
-  },
-  {
-    name: `Bohemian Rhapsody`,
-    pictureSrc: `img/bohemian-rhapsody.jpg`,
-  },
-  {
-    name: `Macbeth`,
-    pictureSrc: `img/macbeth.jpg`,
-  },
-  {
-    name: `Aviator`,
-    pictureSrc: `img/aviator.jpg`,
-  },
-  {
-    name: `We need to talk about Kevin`,
-    pictureSrc: `img/we-need-to-talk-about-kevin.jpg`,
-  },
-  {
-    name: `What We Do in the Shadows`,
-    pictureSrc: `img/what-we-do-in-the-shadows.jpg`,
-  },
-  {
-    name: `Revenant`,
-    pictureSrc: `img/revenant.jpg`,
-  },
-  {
-    name: `Johnny English`,
-    pictureSrc: `img/johnny-english.jpg`,
-  }
-];
-
-const mockfilm = mockFilms.map((el) => {
-  return el;
-});
+const mockfilm =
+{
+  name: `Fantastic Beasts`,
+  pictureSrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+};
 
 describe(`Title click`, () => {
   it(`Should film title be pressed`, () => {
--- src/components/film-card/film-card.test.js
+++ src/components/film-card/film-card.test.js
@@ -2,44 +2,12 @@ import React from "react";
 import renderer from "react-test-renderer";
 import FilmCard from "./film-card.jsx";
 
-const mockFilms = [
-  {
-    name: `Fantastic Beasts`,
-    pictureSrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
-  },
-  {
-    name: `Bohemian Rhapsody`,
-    pictureSrc: `img/bohemian-rhapsody.jpg`,
-  },
-  {
-    name: `Macbeth`,
-    pictureSrc: `img/macbeth.jpg`,
-  },
-  {
-    name: `Aviator`,
-    pictureSrc: `img/aviator.jpg`,
-  },
-  {
-    name: `We need to talk about Kevin`,
-    pictureSrc: `img/we-need-to-talk-about-kevin.jpg`,
-  },
-  {
-    name: `What We Do in the Shadows`,
-    pictureSrc: `img/what-we-do-in-the-shadows.jpg`,
-  },
-  {
-    name: `Revenant`,
-    pictureSrc: `img/revenant.jpg`,
-  },
-  {
-    name: `Johnny English`,
-    pictureSrc: `img/johnny-english.jpg`,
-  }
-];
+const mockfilm =
+{
+  name: `Fantastic Beasts`,
+  pictureSrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+};
 
-const mockfilm = mockFilms.map((el) => {
-  return el;
-});
 
 const mokcFilmTitleHandler = () => {};
 
