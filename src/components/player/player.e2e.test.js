0a556533c62eb4f0c72a82c298a384e433f8a3d3
AlexyShu <Za9i@bk.ru>
2020-06-28 15:06:16 +0300

    добавляет тест для плеера

---
+++ src/components/player/player.e2e.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Enzyme, {mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Player from "./player.jsx";
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+const mockfilm =
+{
+  name: ``,
+  video: ``,
+  genre: ``,
+  posterSrc: ``,
+  year: 2015,
+  ratingScore: ``,
+  ratingLevel: ``,
+  ratingCount: ``,
+  descriptionPartOne: ``,
+  descriptionPartTwo: ``,
+  filmDirector: ``,
+  filmStarring: ``,
+};
+
+it(`Should play video on click`, () => {
+  const fakePlay = jest
+    .spyOn(window.HTMLMediaElement.prototype, `play`)
+    .mockImplementation(() => {});
+
+  const moviePlayer = mount(
+      <Player
+        film = {mockfilm}
+        muted = {true}
+        autoPlay = {false} />
+  );
+
+  expect(moviePlayer.state(`isPlaying`)).toBe(false);
+  moviePlayer.simulate(`click`);
+  expect(moviePlayer.state(`isPlaying`)).toBe(true);
+
+  expect(fakePlay).toHaveBeenCalled();
+  fakePlay.mockRestore();
+});
