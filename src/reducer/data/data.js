087f19e460a5460d7868e6379e767bec4274769c
AlexyShu <Za9i@bk.ru>
2020-07-20 18:06:26 +0300

    Добавляет новый компанент add-review

--- src/reducer/data/data.js
+++ src/reducer/data/data.js
@@ -3,11 +3,13 @@ import {extend, adapterData} from "../../utils.js";
 // данные, объект начального состояния
 const initialState = {
   films: [],
+  comments: []
 };
 
 // Action
 const ActionType = {
   GET_FILMS: `GET_FILMS`,
+  GET_COMMENTS: `GET_COMMENTS`
 };
 
 const ActionCreator = {
@@ -15,6 +17,10 @@ const ActionCreator = {
     type: ActionType.GET_FILMS,
     payload: films
   }),
+  getComments: (comments) => ({
+    type: ActionType.GET_COMMENTS,
+    payload: comments
+  })
 };
 
 const Operation = {
@@ -23,14 +29,35 @@ const Operation = {
     .then((response) => {
       dispatch(ActionCreator.getFilms(adapterData(response.data)));
     });
+  },
+  getComments: () => (dispatch, getState, api) => {
+    return api.get(`/comments/41`)
+    .then((response) => {
+      dispatch(ActionCreator.getComments(response.data));
+    });
+  },
+  addComment: (commentData) => (dispatch, getState, api) => {
+    return api
+      .post(`/comments/41`, {
+        rating: commentData.rating,
+        comment: commentData.comment
+      })
+    .then(() => {
+      dispatch(Operation.getComments());
+    });
   }
 };
+
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.GET_FILMS:
       return extend(state, {
         films: action.payload
+      });
+    case ActionType.GET_COMMENTS:
+      return extend(state, {
+        comments: action.payload
       });
   }
   return state;
